eafdb031f4a93b72b96c9f5bd9e1de9cb25b146e
Juan Carlos Noguera Ramirez <83929119+JuanNogRa@users.noreply.github.com>
2022-06-03 16:48:49 -0500

    Prototipo

--- CallbacksDino.py
+++ CallbacksDino.py
@@ -6,27 +6,32 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
         self.setupUi(self)
-        self.Aplicar.clicked.connect(lambda: self.DACVoltageDC())
         self.SendVoltage = SendVoltage()
         self.ReadVoltage = ReadVoltage()
+        self.Aplicar.clicked.connect(lambda: self.DACVoltageDC())
         self.ReadFrecuency = ReadFrecuency()
-        self.ReadVoltage.start()
+        #self.ReadVoltage.start()
         self.ReadFrecuency.start()
-        self.ReadVoltage.VoltageUpdate.connect(self.VoltageSlotUpdate)
+        #self.ReadVoltage.VoltageUpdate.connect(self.VoltageSlotUpdate)
         self.ReadFrecuency.FrecuencyUpdate.connect(self.FrecuencySlotUpdate)
     
     def DACVoltageDC(self):
-        self.SendVoltage = SendVoltage()
-        if self.SendVoltage.isFinished:
-            self.SendVoltage.start()
-        config.value=self.plainTextEdit.getText()
+        #if self.SendVoltage.isFinished:
+        self.SendVoltage.main_sendVoltage(value)
+        value=self.InputVoltage.text()
+        print('Voltaje '+str(config.value))
 
     def VoltageSlotUpdate(self, Voltage):
-        self.Ch1.setText('{:.5f}'.format(Voltage[0], 'V  '))
-        self.Ch2.setText('{:.5f}'.format(Voltage[1], 'V  '))
-        self.Ch3.setText('{:.5f}'.format(Voltage[2], 'V  '))
-        self.Ch4.setText('{:.5f}'.format(Voltage[3], 'V  '))
+        self.Ch1.setText('{:.5f}'.format(Voltage[0], 'V'))
+        self.Ch2.setText('{:.5f}'.format(Voltage[1], 'V'))
+        self.Ch3.setText('{:.5f}'.format(Voltage[2], 'V'))
+        self.Ch4.setText('{:.5f}'.format(Voltage[3], 'V'))
  
     def FrecuencyUpdate(self, frecuencia):
         self.Frecuency.setText('{:.2f}'.format(frecuencia, 'Hz  '))
     
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    window = MainWindow()
+    window.show()
+    app.exec_()
--- ThreadsDino.py
+++ ThreadsDino.py
@@ -1,93 +1,61 @@
-from PyQt5.QtGui import QImage
-from PyQt5.QtCore import pyqtSignal,QThread, Qt, QMutex 
-from daqhats import mcc152, OptionFlags, HatIDs, HatError
+from PyQt5.QtCore import pyqtSignal,QThread
+from daqhats import mcc152, OptionFlags, HatIDs, HatError, mcc128, AnalogInputMode, AnalogInputRange
 from daqhats_utils import select_hat_device
-from sys import version_info
-from __future__ import print_function
 import config
 
 """Hilo para generar voltaje usando el MCC 152."""
-class SendVoltage(QThread):
-    
-    def __init__(self, path, activateRectification):
-        QThread.__init__(self)
+class SendVoltage():
+    def main_sendVoltage(self, value):
         """
         This function is executed automatically when the module is run directly.
         """
-        self.options = OptionFlags.DEFAULT
-        self.channel = 0
+        options = OptionFlags.DEFAULT
+        channel = 0
         num_channels = mcc152.info().NUM_AO_CHANNELS
 
         # Ensure channel is valid.
-        if self.channel not in range(num_channels):
+        if channel not in range(num_channels):
             error_message = ('Error: Invalid channel selection - must be '
                             '0 - {}'.format(num_channels - 1))
             raise Exception(error_message)
         # Get an instance of the selected hat device object.
         address = select_hat_device(HatIDs.MCC_152)
-        self.hat = mcc152(address)
+        hat = mcc152(address)
         
-        
-    def run(self):
-        self.ThreadActive = True
-        error = False
-        while self.ThreadActive and not error:
-            try:
-                value = self.get_input_value()
-            except ValueError:
-                self.ThreadActive = False
-            else:
-                # Write the value to the selected channel.
-                try:
-                    self.hat.a_out_write(channel=self.channel,
-                                value=value,
-                                options=self.options)
-                except (HatError, ValueError):
-                    error = True
+        #while self.ThreadActive and not error:
+        value = self.get_input_value(value)
+        # Write the value to the selected channel.
+        try:
+            hat.a_out_write(channel=channel,
+                            value=value,
+                            options=options)
+        except (HatError, ValueError):
+            error = True
         if error:
             print("Error al escribir en la salida analoga.")
-            
-    def stop(self):
-        self.ThreadActive = False
-        self.quit()
-
-    def get_input_value(self):
+                    
+    def get_input_value(self, value):
         """Get the voltage from the user and validate it."""
 
         # Get the min and max voltage values for the analog outputs to validate
         # the user input.
         min_v = mcc152.info().AO_MIN_RANGE
         max_v = mcc152.info().AO_MAX_RANGE
-        message=config.value
-        if version_info.major > 2:
-            str_v = input(message)
+        message=value
+        value = float(message)
+        value = value/100.0
+        #print('Voltage en hilo '+str(value))
+        if (value < min_v) or (value > max_v):
+            # Out of range, ask again.
+            print("Value out of range.")
         else:
-            str_v = raw_input(message)
-
-        try:
-            value = float(str_v)
-            value = value/100.0
-        except ValueError:
-            raise
-        else:
-            if (value < min_v) or (value > max_v):
-                # Out of range, ask again.
-                print("Value out of range.")
-            else:
-                # Valid value.
-                return value
-
-from time import sleep
-from sys import stdout
-from daqhats import mcc128, OptionFlags, HatIDs, HatError, AnalogInputMode, \
-    AnalogInputRange
-from daqhats_utils import select_hat_device, enum_mask_to_string, \
-    input_mode_to_string, input_range_to_string
+            # Valid value.
+            return value
 
 """Hilo para leer voltaje usando el MCC 128."""
 class ReadVoltage(QThread):
-    VoltageUpdate = pyqtSignal()
-    def __init__(self, path, activateRectification):
+    VoltageUpdate = pyqtSignal(list)
+    def __init__(self):
         QThread.__init__(self)
         """
         This function is executed automatically when the module is run directly.
@@ -135,7 +103,7 @@ class ReadVoltage(QThread):
         while self.ThreadActive and not error:
             # Display the updated samples per channel count
                 samples_per_channel += 1
-                print('\r{:17}'.format(samples_per_channel), end='')
+                #print('\r{:17}'.format(samples_per_channel), end='')
 
                 # Read a single value from each selected channel.
                 for chan in canales_lectura:
@@ -148,11 +116,14 @@ class ReadVoltage(QThread):
                         sumCh2=sumCh2+value
                     if (chan==5):
                         sumCh3=sumCh3+value
-                    print('{:12.5} V'.format(value), end='')
-
-                stdout.flush()
+                    #print('{:12.5} V'.format(value), end='')
                 if (samples_per_channel==100):
-                    self.VoltageUpdate.emit([sumCh0/100, sumCh1/100, sumCh2/100, sumCh3/100])
+                    samples_per_channel=0
+                    self.VoltageUpdate.emit([sumCh0/samples_per_channel, sumCh1/samples_per_channel, sumCh2/samples_per_channel, sumCh3/samples_per_channel])
+                    sumCh0=0
+                    sumCh1=0
+                    sumCh2=0
+                    sumCh3=0
             
     def stop(self):
         self.ThreadActive = False
@@ -161,6 +132,7 @@ class ReadVoltage(QThread):
 import time
 import pigpio
 import read_PWM
+
 """Hilo para leer frecuencia usando la libreria Pigpio."""
 class ReadFrecuency(QThread):
     FrecuencyUpdate = pyqtSignal()
@@ -170,36 +142,8 @@ class ReadFrecuency(QThread):
     happens per second.  The duty cycle is the percentage of
     pulse high time per cycle.
     """
-    def __init__(self, pi, gpio, weighting=0.0):
+    def __init__(self):
         QThread.__init__(self)
-        """
-        Instantiate with the Pi and gpio of the PWM signal
-        to monitor.
-
-        Optionally a weighting may be specified.  This is a number
-        between 0 and 1 and indicates how much the old reading
-        affects the new reading.  It defaults to 0 which means
-        the old reading has no effect.  This may be used to
-        smooth the data.
-        """
-        self.pi = pi
-        self.gpio = gpio
-
-        if weighting < 0.0:
-            weighting = 0.0
-        elif weighting > 0.99:
-            weighting = 0.99
-
-        self._new = 1.0 - weighting # Weighting for new reading.
-        self._old = weighting       # Weighting for old reading.
-
-        self._high_tick = None
-        self._period = None
-        self._high = None
-
-        pi.set_mode(gpio, pigpio.INPUT)
-
-        self._cb = pi.callback(gpio, pigpio.EITHER_EDGE, self._cbf)
         
         PWM_GPIO = 4
         self.SAMPLE_TIME = 2.0
